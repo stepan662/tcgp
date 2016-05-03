abb383c77bfc61b70e239340b3605d4c0adbda4f
stepan <granat.stepan@gmail.com>
2016-05-03 15:58:57 +0200

    makecd created

--- makecd.sh
+++ makecd.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
-rm -rf .cd
-echo "Creating CD structure archive ..."
+echo "Creating CD structure ..."
 mkdir .cd
 mkdir .cd/src
 mkdir .cd/docs
+mkdir .cd/docs/fig
+mkdir .cd/tests
+mkdir .cd/tests/outputs
+
+echo "Copying files ..."
+cp docs/*.tex .cd/docs/
+cp docs/*.bib .cd/docs/
+cp docs/*.bst .cd/docs/
+cp docs/fitthesis.cls .cd/docs/
+cp docs/fig/* .cd/docs/fig/
+cp docs/Makefile .cd/docs/
+cp README.* .cd/
 cp *.py .cd/src/
-cd docs
+cp ../tcgp-tests/*.in .cd/tests
+cp ../tcgp-tests/test.sh .cd/tests
+# change relative position in test.sh file
+sed -i -e 's/BINARY="python3 ..\/tcgp\/tcgp.py"/BINARY="python3 ..\/src\/tcgp.py"/g' .cd/tests/test.sh
+
 echo "Building documentation ..."
+cd .cd/docs
 make > /dev/null
-cp projekt.pdf ../.cd/
+cp thesis.pdf ../
 make clean > /dev/null
 cd ..
-cp docs/{czechiso.bst, fitthesis.cls, *.tex, *.bib, *.bst, fig/*, Makefile}
-cp README.* .cd/
-#tar czvf bp-xgrana02.tar.gz czechiso.bst fitthesis.cls *.tex *.bib *.bst ./fig/* Makefile
-#rm -rf .cd
+
+echo "Building archive ..."
+tar czvf ../bp-xgrana02.tar.gz * >/dev/null
+
+cd ..
+
+echo "Removing temporary files ..."
+rm -r -f ".cd"
